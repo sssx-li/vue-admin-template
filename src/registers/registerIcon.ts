28197bf42915cc1ed4daca75d1d717df03d94aa6
lizhiquan <842048365@qq.com>
2022-09-06 22:29:40 +0800

    perf: 移除路由/菜单中无用字段url/pid

--- src/registers/registerIcon.ts
+++ src/registers/registerIcon.ts
@@ -1,17 +1,17 @@
-import { App } from 'vue';
-import {
-  Avatar,
-  CircleClose,
-  HomeFilled,
-  Menu,
-  Tools,
-  Promotion
-} from '@element-plus/icons-vue';
+// import { App } from 'vue';
+// import {
+//   Avatar,
+//   CircleClose,
+//   HomeFilled,
+//   Menu,
+//   Tools,
+//   Promotion
+// } from '@element-plus/icons-vue';
 
-const components = [Avatar, CircleClose, HomeFilled, Menu, Tools, Promotion];
+// const components = [Avatar, CircleClose, HomeFilled, Menu, Tools, Promotion];
 
-export default function (app: App): void {
-  for (const icon of components) {
-    app.component(icon.name, icon);
-  }
-}
+// export default function (app: App): void {
+//   for (const icon of components) {
+//     app.component(icon.name, icon);
+//   }
+// }
