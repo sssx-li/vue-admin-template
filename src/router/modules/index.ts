3d70cf86128774bc86bcb271c35a28faaf6bead6
lizhiquan <842048365@qq.com>
2022-09-06 22:42:55 +0800

    perf: 优化路由结构

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -1,12 +1,10 @@
 import { RouteRecordRaw } from 'vue-router';
 
-const mainStaticRoutes: Array<RouteRecordRaw> = [
-  {
-    path: '/main/home',
-    name: 'home',
-    meta: { title: '首页', icon: 'HomeFilled', sort: 1 },
-    component: () => import('@/views/main/home/index.vue')
-  }
-];
+const mainStaticRoute: RouteRecordRaw = {
+  path: '/main/home',
+  name: 'home',
+  meta: { title: '首页', icon: 'HomeFilled', sort: 1 },
+  component: () => import('@/views/main/home/index.vue')
+};
 
-export { mainStaticRoutes };
+export default mainStaticRoute;
--- src/router/modules/main/about.ts
+++ src/router/modules/main/about.ts
@@ -1,36 +1,33 @@
 import { RouteRecordRaw } from 'vue-router';
 
-const aboutRoutes: Array<RouteRecordRaw> = [
-  {
-    path: '/main/about',
-    name: 'about',
-    redirect: '/main/about/first',
-    meta: {
-      title: '关于',
-      icon: 'FolderOutlined',
-      sort: 2
-    },
-    children: [
-      {
-        path: '/main/about/first',
-        name: 'aboutFirst',
-        component: () => import('@/views/main/about/index.vue'),
-        meta: {
-          title: '关于1-1',
-          icon: 'SnippetsOutlined'
-        }
-      },
-      {
-        path: '/main/about/secound',
-        name: 'aboutSecound',
-        component: () => import('@/views/main/about/secound.vue'),
-        meta: {
-          title: '关于1-2',
-          icon: 'SnippetsOutlined'
-        }
+const aboutRoute: RouteRecordRaw = {
+  path: '/main/about',
+  name: 'about',
+  redirect: '/main/about/first',
+  meta: {
+    title: '关于',
+    icon: 'FolderOutlined',
+    sort: 2
+  },
+  children: [
+    {
+      path: '/main/about/first',
+      name: 'aboutFirst',
+      component: () => import('@/views/main/about/index.vue'),
+      meta: {
+        title: '关于1-1',
+        icon: 'SnippetsOutlined'
       }
-    ]
-  }
-];
-
-export default aboutRoutes;
+    },
+    {
+      path: '/main/about/secound',
+      name: 'aboutSecound',
+      component: () => import('@/views/main/about/secound.vue'),
+      meta: {
+        title: '关于1-2',
+        icon: 'SnippetsOutlined'
+      }
+    }
+  ]
+};
+export default aboutRoute;
