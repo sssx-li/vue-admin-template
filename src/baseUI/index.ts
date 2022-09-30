62e4c27a5b82431c9880e1c270d40aea119c0000
lizq <lizq@epshealth.com>
2022-09-30 18:34:03 +0800

    feat: modal组件封装

--- src/baseUI/index.ts
+++ src/baseUI/index.ts
@@ -3,5 +3,6 @@ import SyDrawer from './syDrawer/index.vue';
 import SyEchart from './syEchart/index.vue';
 import SyForm from './syForm/index.vue';
 import SyTable from './syTable/index.vue';
+import SyModal from './SyModal/index.vue';
 
-export { SyCard, SyDrawer, SyEchart, SyForm, SyTable };
+export { SyCard, SyDrawer, SyEchart, SyForm, SyTable, SyModal };
